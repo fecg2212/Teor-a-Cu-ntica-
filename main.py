5dd80786674d77e88707909f9cdbb7327c8fb0bd
fecg2212 <111906026+fecg2212@users.noreply.github.com>
2022-09-27 13:27:28 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,38 @@
+def nomoduloCplx(cplx):
+    # Saca el modulo de un complejo representado como una tupla
+    module = ((cplx[0] ** 2 + (cplx[1] ** 2)))
+    return module
+
+def normaVector(v):
+    suma = []
+    for i in range(len(v)):
+        mod = nomoduloCplx(v[i])
+        suma.append(mod)
+    return sum(list(map(int, suma)))
+
+def superposition(vec, pos):
+    c = nomoduloCplx(vec[pos])
+    norma = normaVector(vec)
+    super = c / norma
+    return round(super, 6)
+
+vec = [(-3, 1), (0, -2), (0, 1), (2, 0)]
+print(superposition(vec, 2))
+
+def prob(vec1, vec2):
+    pro_int = np.inner(vec1, vec2)
+    print(pro_int)
+    n1 = 0
+    n2 =0
+    for i in range(len(vec1)):
+        n = abs(vec1[i])
+        n1 += n
+    for i in range(len(vec2)):
+        n = abs(vec2[i])
+        n2 += n
+    normas = n1 * n2
+    return pro_int / normas
+
+
+
+
